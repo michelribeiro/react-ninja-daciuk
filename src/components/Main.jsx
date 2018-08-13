c8700a27fef2184ce135fae30bbe1fe5264a685a
michel ribeiro <michelribeiro2004@gmail.com>
2018-08-13 18:11:42 -0300

    exemplo real

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,32 @@
 import React, { Component } from "react";
 import { Table } from 'react-bootstrap';
-//Grid, Row, Col,Glyphicon,
-import data from '../db.json';
+
+
+import {config, path} from '../config/api'
 
 class Main extends Component {
 
     state = {
-        data: []
+        data: [],
+        rows: []
+    }
+
+    setApiList() {
+        fetch(config.API_BASE_URL+path.LOGS)
+        .then(response => response.json())
+        .then(rows => this.setState({rows}))
+        .catch(error => console.log(error))
     }
 
     componentDidMount() {
-        this.setState({ data })
+        // this.setState({ data })
+        this.setApiList();
     }
     // console.log('---->', i.LOSE_DS_URL.replace("https://modelosite/api/", "").replace("http://localhost/api/", ""))
     // console.log(url.replace(/(http:\/\/).{0,100}(\/api\/)/g, 'api/'))
  
     render() {
+
         return (
             <div>
                 <Table striped bordered responsive hover>
@@ -33,11 +44,11 @@ class Main extends Component {
                     </thead>
                     <tbody>
                         {
-                            this.state.data.map((data, key) => {
+                            this.state.rows.map((data, key) => {
                                 return (
                                     <tr key={key}>
                                         <td>{data.LOSE_ID_LOG_SERVICO_API}</td>
-                                        <td>{data.LOSE_DS_URL.replace("https://modelosite/api/", "").replace("http://localhost/api/", "")}</td>
+                                        <td>{data.LOSE_DS_URL.replace("https://tst.icatuseguros.com.br/api/", "").replace("http://localhost/api/", "")}</td>
                                         <td>{data.LOSE_TP_RESQUEST_METHOD}</td>
                                         <td>{data.LOSE_DS_NOME_SISTEMA}</td>
                                         <td className={data.LOSE_CD_STATUS_CODE !== '200' ? 'error': 'normal'}>
--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,3 +1,6 @@
 export const config = {
-    API_BASE_URL: "http://localhost:54753/" // esse modelo é rodando a api local
+    API_BASE_URL: "http://localhost:54753"
+}
+export const path = {
+    LOGS: "/logs"
 }
